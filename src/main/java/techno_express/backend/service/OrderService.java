6f01a6af7f37744fcde3c889c5c933e8b169e9b3
Francisco Solé <franciscosoleescande@gmail.com>
2025-08-11 14:07:46 -0300

    refactor(OrderService): agregar validaciones
    Se agregaron validaciones para controlar mejor cada item perteneciente a la orden enviada a guardar en la db, si alguno de los campos es invalido se cortara la ejecucion

--- src/main/java/techno_express/backend/service/OrderService.java
+++ src/main/java/techno_express/backend/service/OrderService.java
@@ -11,6 +11,7 @@ import techno_express.backend.dto.OrderUserResponseDto;
 import techno_express.backend.entity.*;
 import techno_express.backend.exception.CartException;
 import techno_express.backend.exception.OrderException;
+import techno_express.backend.exception.ProductException;
 import techno_express.backend.exception.UserException;
 import techno_express.backend.repository.*;
 
@@ -35,6 +36,14 @@ public class OrderService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    private static boolean isBlank(String s) {
+        return s == null || s.trim().isEmpty();
+    }
+    private static final List<String> VALID_CATEGORIES = List.of("perifericos", "computacion", "electrodomesticos");
 
     @Transactional
     public void create_order(String request_id, OrderRequestDto orderRequestDto) {
@@ -102,6 +111,68 @@ public class OrderService {
 
         logger.info(request_id + " - guardando articulos de la orden...");
         for (OrderItem item : orderRequestDto.getItems()) {
+            String item_category = item.getCategory();
+            String item_name = item.getName();
+            String item_description = item.getDescription();
+            double item_price = item.getPrice();
+            int item_quantity = item.getQuantity();
+            User item_seller = item.getSeller();
+            Long item_seller_id =  item_seller.getId();
+            Product item_product = item.getProduct();
+            Long item_product_id =  item_product.getId();
+
+            if(isBlank(item_category)){
+                logger.error(request_id + " - la categoria enviada esta vacia");
+                throw new OrderException.InvalidData();
+            }
+
+            if (!VALID_CATEGORIES.contains(item_category.toLowerCase())) {
+                logger.error(request_id + " - la categoría '" + item_category + "' es inválida");
+                throw new OrderException.InvalidData();
+            }
+
+            if(isBlank(item_name)){
+                logger.error(request_id + " - el nombre enviado esta vacio");
+                throw new OrderException.InvalidData();
+            }
+
+            if(isBlank(item_description)){
+                logger.error(request_id + " - la descripcion enviada esta vacia");
+                throw new OrderException.InvalidData();
+            }
+
+            if(item_price < 1){
+                logger.error(request_id + " - el precio enviado es menor a 1 ('{}')", item_price);
+                throw new OrderException.InvalidData();
+            }
+
+            if(item_quantity < 1){
+                logger.error(request_id + " - la cantidad enviada es menor a 1 ('{}')", item_quantity);
+                throw new OrderException.InvalidData();
+            }
+
+            if(item_seller_id < 1){
+                logger.error(request_id + " - el id de usuario enviado es menor a 1 ('{}')", item_seller_id);
+                throw new UserException.InvalidData();
+            }
+
+            Optional<User> seller = userRepository.findById(item_seller_id);
+            if (seller.isEmpty()) {
+                logger.error(request_id + " - el usuario id '{}' no existe", item_seller_id);
+                throw new UserException.NotFound();
+            }
+
+            if(item_product_id < 1){
+                logger.error(request_id + " - el id de producto enviado es menor a 1 ('{}')", item_product_id);
+                throw new OrderException.InvalidData();
+            }
+
+            Optional<Product> product = productRepository.findById(item_product_id);
+            if (product.isEmpty()) {
+                logger.error(request_id + " - el producto id '{}' no existe", item_product_id);
+                throw new ProductException.NotFound();
+            }
+
             OrderItem orderItem = new OrderItem();
             orderItem.setCategory(item.getCategory());
             orderItem.setDescription(item.getDescription());
@@ -117,9 +188,9 @@ public class OrderService {
     }
 
     public Map<Long, OrderUserResponseDto> get_order_by_user_id(String request_id, OrderRequestDto orderRequestDto) {
+        logger.info(request_id + " - validando interfaces recibidas...");
         Long user_id = orderRequestDto.getUser_id();
 
-        logger.info(request_id + " - validando interfaces recibidas...");
         if(user_id < 1){
             logger.error(request_id + " - el usuario es invalido, enviaron el id '{}'", user_id);
             throw new UserException.InvalidData();
